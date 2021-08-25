2e7b380f82e79e9fb40d399026d25df4dd97929e
Marta Karbowska <karbowana.marta@gmail.com>
2021-08-25 15:23:25 +0200

    Added function to fetch and change user details

--- src/js/index.js
+++ src/js/index.js
@@ -4,4 +4,20 @@ window.onload = function () {
     .then((data) => fetchData(data));
 };
 
-console.log("Hello world");
+function fetchData(data) {
+  var result = data.results[0];
+  var realname = result.name.title + " " + result.name.first + " " + result.name.last;
+  document.getElementById("realname").innerHTML = realname;
+
+  var image = result.picture.large;
+  document.getElementById("avatar").src = image;
+
+  var username = "@" + result.login.username;
+  document.getElementById("username").innerHTML = username;
+
+  var email = result.email;
+  document.getElementById("email").innerHTML = email;
+
+  var country = result.location.country;
+  document.getElementById("country").innerHTML = country;
+}
