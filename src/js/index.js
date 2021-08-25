60d9a7055d00541a266b1f1e9a7a86d522a5ae12
Marta Karbowska <karbowana.marta@gmail.com>
2021-08-25 15:16:36 +0200

    Added API from randomuser

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,7 @@
-import "../scss/style.scss";
+window.onload = function () {
+  fetch("https://randomuser.me/api")
+    .then((response) => response.json())
+    .then((data) => fetchData(data));
+};
 
 console.log("Hello world");
